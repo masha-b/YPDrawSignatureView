3d91881952a84a24888ff0d4577655b1d270bf2f
masha-b <maria@sitesoft.ru>
2022-02-22 13:07:05 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,16 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "YPDrawSignature",
-    platforms: [.iOS(.v11)],
     products: [
         .library(name: "YPDrawSignature", targets: ["YPDrawSignature"])
     ],
     targets: [
       .target(
-        name: "YPDrawSignature",
-        path: "",
-        sources: ["Sources"]
+        name: "YPDrawSignature"
       )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
